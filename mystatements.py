7fc0be9f724a0d5141c48d55b424ede98879657b
Yash9-star <YASWANTH.SEETHAGARI2@GMAIL.COM>
2025-01-08 05:50:56 +0000

    adding files to repo

---
+++ mystatements.py
@@ -0,0 +1,11 @@
+my_list = ["apple", "cherry", "lemon"]
+my_dict = {"drink": "coffee", "milk": "whole"}  # Use curly braces for dictionaries
+
+for item in my_list:
+    print(f"My favourite thing to eat is: {item}")  # Remove the unnecessary parenthesis
+
+for _,value in my_dict.items():
+    print(f"My favourite thing to eat is: {value}")
+
+
+
